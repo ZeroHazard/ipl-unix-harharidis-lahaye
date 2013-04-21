b353a574b1e0f25f9544efeb20ab65c01f3edbb3
lemauve009@gmail.com <lemauve009@gmail.com@3bac5053-232d-90a4-18b7-461c6a767aaf>
2013-04-21 18:10:00 +0000

    Ajout du header

---
+++ header.h
@@ -0,0 +1,26 @@
+#ifndef Unix_header_h
+#define Unix_header_h
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+#define tailleLigne 1024
+
+#include <stdio.h>
+#include <stdlib.h>
+#define PORT 50000
+
+typedef struct client{
+    char pseudo[50];
+    SOCKET csocket;
+}client;
+
+#endif
