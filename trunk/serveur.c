7a541c4e6ead659b713910bcc727beefa6270916
lemauve009@gmail.com <lemauve009@gmail.com@3bac5053-232d-90a4-18b7-461c6a767aaf>
2013-04-21 18:10:18 +0000

    Ajout du serveur

---
+++ trunk/serveur.c
@@ -0,0 +1,134 @@
+#include "header.h"
+#include <signal.h>
+#define JOUEURS_MAX 5
+#define TEMPS 30
+typedef struct tabClient{
+    client* clients;
+    int tailleLogique;
+}tabClient;
+void handler(int);
+int boolean = 1;
+int main(void)
+{
+    int erreur = 0;
+    int i;
+    tabClient tabClient;
+    client c;
+    client* ptr;
+    if((ptr = tabClient.clients = (client*)malloc(JOUEURS_MAX*sizeof(client)))==NULL){
+        perror("problem malloc?");
+        exit(1);
+    }
+    tabClient.tailleLogique = 0;
+    /* Socket et contexte d'adressage du serveur */
+    SOCKADDR_IN sin;
+    SOCKET sock;
+    socklen_t recsize = sizeof(sin);
+    
+    /* Socket et contexte d'adressage du client */
+    SOCKADDR_IN csin;
+    SOCKET csock = 0;
+    socklen_t crecsize = sizeof(csin);
+    
+    int sock_err;
+    
+    
+    if(!erreur)
+    {
+        /* Création d'une socket */
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        
+        /* Si la socket est valide */
+        if(sock != INVALID_SOCKET)
+        {
+            //printf("La socket %d est maintenant ouverte en mode TCP/IP\n", sock);
+            
+            /* Configuration */
+            sin.sin_addr.s_addr = htonl(INADDR_ANY);  /* Adresse IP automatique */
+            sin.sin_family = AF_INET;                 /* Protocole familial (IP) */
+            sin.sin_port = htons(PORT);               /* Listage du port */
+            sock_err = bind(sock, (SOCKADDR*)&sin, recsize);
+            
+            /* Si la socket fonctionne */
+            if(sock_err != SOCKET_ERROR)
+            {
+                /* Démarrage du listage (mode server) */
+                sock_err = listen(sock, JOUEURS_MAX);
+                //printf("Listage du port %d...\n", PORT);
+                
+                /* Si la socket fonctionne */
+                if(sock_err != SOCKET_ERROR)
+                {
+                    /* Attente pendant laquelle le client se connecte */
+                    printf("Patientez pendant que le client se connecte sur le port %d...\n", PORT);
+                    struct sigaction sa;
+                    sa.sa_handler = handler;
+                    sigemptyset(&sa.sa_mask);
+                    sa.sa_flags = 0;
+                    sigaction(SIGALRM,&sa,NULL);
+                    while(boolean == 1){
+                        //Si c'est le premier inscrit on lance l'alarm
+                        // La condition du while est un boolean qu'on mettra a fals dans la methode handler
+                        //Gerer la deconnexion d'un joueur en inscription
+                        if(tabClient.tailleLogique == 0){
+                            alarm(TEMPS);
+                        }
+                        if((csock = accept(sock, (SOCKADDR*)&csin, &crecsize))<0){
+                            break;
+                        }
+                        printf("Un client se connecte avec la socket %d de %s:%d\n", csock, inet_ntoa(csin.sin_addr), htons(csin.sin_port));
+                        if(recv(csock, &c, sizeof(c), 0)<0){
+                            perror("Problem recv?");
+                            exit(1);
+                        }
+                        ptr = tabClient.clients+tabClient.tailleLogique;
+                        strcpy(ptr->pseudo,c.pseudo);
+                        ptr->csocket = csock;
+                        printf("Vous avez bien été inscrit %s\n", ptr->pseudo);
+                        tabClient.tailleLogique++;
+                    }
+                    //On regarde si tous le clients sont toujours connectés
+                    for (i=0; i<tabClient.tailleLogique; i++) {
+                        char ligne[1024]= "";
+                        if((recv((tabClient.clients+i)->csocket, ligne, sizeof(ligne), MSG_DONTWAIT))==0){
+                            printf("Deconnexion du joueur: %s", (tabClient.clients+i)->pseudo);
+                            if(i==tabClient.tailleLogique){
+                                ptr--;
+                            }else{
+                                client ptr3 = *(tabClient.clients+tabClient.tailleLogique-1);
+                                *(tabClient.clients+i) = ptr3;
+                                ptr--;
+                                i--;
+                            }
+                            tabClient.tailleLogique--;
+                        }
+                    }
+                    if(tabClient.tailleLogique >= 2){
+                        printf("La partie commence!\n");
+                    } else {
+                        printf("Il n'y a pas assez de joueur pour commencer la partie\n");
+                    }
+                }
+                else
+                    perror("listen");
+            }
+            else
+                perror("bind");
+            
+            /* Fermeture de la socket client et de la socket serveur */
+            close(csock);
+            close(sock);
+            printf("Fermeture du serveur terminée\n");
+        }
+        else
+            perror("socket");
+        
+    }
+    
+    return EXIT_SUCCESS;
+}
+
+
+void handler(int null){
+	boolean = 0;
+}
