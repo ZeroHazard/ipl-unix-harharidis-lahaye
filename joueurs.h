24760d44be811cab7be12c8596e63ef61e4b8582
hhds.mathieu@gmail.com <hhds.mathieu@gmail.com@3bac5053-232d-90a4-18b7-461c6a767aaf>
2013-04-25 17:25:25 +0000

    ajout de joueur/partie/message/memoire pour l'écriture

---
+++ joueurs.h
@@ -0,0 +1,17 @@
+/*
+ * joueurs.h
+ *
+ *  Created on: Apr 25, 2013
+ *      Author: afro
+ */
+
+#ifndef JOUEURS_H_
+#define JOUEURS_H_
+
+typedef struct joueurs{
+    char pseudo[50];
+    int score;
+}joueurs;
+
+
+#endif /* JOUEURS_H_ */
--- memoire.h
+++ memoire.h
@@ -1,7 +1,16 @@
 #ifndef ipl_lahaye_harharidis_memoire_h
 #define ipl_lahaye_harharidis_memoire_h
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
+#define MEMOIRE 19
+
+#include <partie.h>
+
 int initMemoire();
 #endif
+
+partie* lectureMemoire(int qid);
+void ecritureMemoire(int, partie *);
+void removeMemoire(int, int);
