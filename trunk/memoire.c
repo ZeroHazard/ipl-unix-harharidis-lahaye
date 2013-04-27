660e6581e490b83e0ee4c7b1b0236de0e29b680f
lemauve009@gmail.com <lemauve009@gmail.com@3bac5053-232d-90a4-18b7-461c6a767aaf>
2013-04-27 12:28:00 +0000

    Ajout de la lecture et écriture dans la mémoire partagée

--- trunk/memoire.c
+++ trunk/memoire.c
@@ -1,40 +1,44 @@
 #include "memoire.h"
-#include "util.h"
 
 
 struct sembuf memOp;
 int mem;
+key_t key;
+int shmid;
+partie* part;
 
-int initMemoire(int fd_erreur){
-    
-    int shmid;
-    key_t key = 5678;
-    char* shm;
-    partie* p;
-    if((shmid = shmget(key, sizeof(p), IPC_CREAT | 0666)<0)){
-        afficher_erreur(fd_erreur, "memoire-shmget");
+
+
+void initMemoire(int fd_erreur, int serveur){
+//    part = initPartie(part);
+    key = ftok("./", 42);
+    if(serveur == 1){
+        if((shmid = shmget(key, sizeof(part), IPC_CREAT | 0666))<0){
+            afficher_erreur(fd_erreur, "memoire-shmget\n");
+            part->nombreJoueur = 0;
+        }
+    }else{
+        if((shmid = shmget(key, sizeof(part), 0))<0){
+            afficher_erreur(fd_erreur, "memoire-shmget\n");
+        }
     }
-    
-    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1){
-        afficher_erreur(fd_erreur, "memoire-shmat");
+    part = shmat(shmid, NULL, 0);
+    if ((int) part < 0) {
+        afficher_erreur(fd_erreur, "memoire-shmat\n");
     }
-    return shmid;
 }
 
-//écrit dans le mémoire
-void ecritureMemoire(int qid, partie * p, int fd_erreur){
-    partie* ptr;
-    key_t key = 5678;
-    int shmid;
-    if((shmid = shmget(key, sizeof(p), IPC_CREAT | 0666)<0)){
-        afficher_erreur(fd_erreur, "memoire-shmget");
-    }
-    if((ptr = shmat(shmid, NULL, 0)) == (void*) -1){
-        perror("Une erreur lors de la lecture de la mémoire");
-        exit(-1);
-    }
-    p->nombreJoueur = 2;
-    *ptr = *p;
+//écrit dans la mémoire
+void ecritureMemoire(int fd_erreur, client* cl){
+    joueurs* j;
+    j = initJoueurs(cl->pseudo, fd_erreur);
+    part->nombreJoueur = part->nombreJoueur+1;
+    part->joueurs[part->nombreJoueur-1] = *j;
+    printf("SERVEUR-Le nombre de joueur est :%d\n",part->nombreJoueur);
+    printf("SERVEUR-Le pseudo du joueur est :%s\n",(part->joueurs)[0].pseudo);
+    printf("SERVEUR-Le pseudo du joueur est :%s\n",(part->joueurs)[1].pseudo);
+
+//    printf("SERVEUR-Le nombre de joueur est de : %d\n", part->nombreJoueur);
     //    partie * ptr;
     //    if((ptr = (partie *) shmat(qid,NULL,0))==(partie *) -1){
     //        perror("Une erreur lors de la lecture de la mémoire");
@@ -66,18 +70,11 @@ void ecritureMemoire(int qid, partie * p, int fd_erreur){
     //        fprintf(stderr,"ecriture12\n");
 }
 
-partie* lectureMemoire(int qid, int fd_error){
-    partie * ptr;
-    key_t key = 5678;
-    
-    int id;
-    if((id = shmget(key, sizeof(partie), 0444))<0){
-        afficher_erreur(fd_error, "memoire-shmget");
-    }
-    if((ptr = (partie*) shmat(id, NULL, 0))){
-        afficher_erreur(fd_error, "memoire-shmat");
-    }
-    return ptr;
+partie* lectureMemoire(int fd_error){
+    printf("Le nombre de joueur est :%d\n",part->nombreJoueur);
+    printf("Le pseudo du joueur est :%s\n",(part->joueurs)[0].pseudo);
+    printf("Le pseudo du joueur est :%s\n",(part->joueurs)[1].pseudo);
+    return part;
     //	memOp.sem_num = 0;
     //	memOp.sem_flg = 0;
     //	sop.sem_num = 0;
@@ -106,4 +103,14 @@ partie* lectureMemoire(int qid, int fd_error){
     //	sop.sem_op = 1; //up
     //	SYS(semop(mutex,&sop,2)); // zone non critique
     //    return ptr;
+    perror("BLABLA");
+}
+
+void fermerMemoire(int fd_error){
+    if((shmdt(part))<0){
+        afficher_erreur(fd_error, "memoire-shmdt\n");
+    }
+    if((shmctl(shmid, IPC_RMID, NULL))<0){
+        afficher_erreur(fd_error, "memoire-shmctl");
+    }
 }
